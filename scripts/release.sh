3bb0df228797be9e476cd57fd4f9c4d76008f686
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-01-02 10:59:04 -0500

    Create brew formula

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-function bump() {
-  output=$(npm version ${release} --no-git-tag-version)
-  version=${output:1}
+function bump_source() {
   search='(let version = ").+(")'
-  replace="\1${version}\2"
-
+  replace="\1$2\2"
   sed -i ".tmp" -E "s/${search}/${replace}/g" "$1"
   rm "$1.tmp"
+}
+
+function bump_brew() {
+  search="(VERSION = ').+(')"
+  replace="\1$2\2"
+  sed -i ".tmp" -E "s/${search}/${replace}/g" "$1"
+  rm "$1.tmp"
+}
+
+function bump_all() {
+  output=$(npm version "${release}" --no-git-tag-version)
+  version=${output:1}
+  bump_source "bin/package.re" "$version"
+  bump_brew "scripts/spin.rb" "$version"
 }
 
 function help() {
@@ -25,7 +36,7 @@ if [ -d ".git" ]; then
   changes=$(git status --porcelain)
 
   if [ -z "${changes}" ]; then
-    bump "bin/package.re"
+    bump_all
     git add .
     git commit -m "Bump to ${version}"
     git tag -a "${output}" -m "${version}"
@@ -34,5 +45,5 @@ if [ -d ".git" ]; then
     echo "Please commit staged files prior to bumping"
   fi
 else
-  bump "bin/package.re"
+  bump_all
 fi
