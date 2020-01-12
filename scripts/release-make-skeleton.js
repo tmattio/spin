8b65d1088618751304a319011f897b1f6eece461
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-01-11 21:47:10 -0500

    Change release to use a postinstall script with a dummy file

---
+++ scripts/release-make-skeleton.js
@@ -0,0 +1,73 @@
+// @flow
+
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const package = require('../package.json');
+
+function exec(cmd) {
+  console.log(`exec: ${cmd}`);
+  return execSync(cmd).toString();
+}
+
+function mkdirpSync(p) {
+  if (fs.existsSync(p)) {
+    return;
+  }
+  mkdirpSync(path.dirname(p));
+  fs.mkdirSync(p);
+}
+
+function removeSync(p) {
+  exec(`rm -rf "${p}"`);
+}
+
+const src = path.resolve(path.join(__dirname, '..'));
+const dst = path.resolve(path.join(__dirname, '..', '_release'));
+
+removeSync(dst);
+mkdirpSync(dst);
+
+const filesToCopy = ['LICENSE', 'README.md'];
+
+for (const file of filesToCopy) {
+  const p = path.join(dst, file);
+  mkdirpSync(path.dirname(p));
+  fs.copyFileSync(path.join(src, file), p);
+}
+
+fs.copyFileSync(
+  path.join(src, 'scripts', 'release-postinstall.js'),
+  path.join(dst, 'postinstall.js')
+);
+
+const filesToTouch = [
+  'spin.exe'
+];
+
+for (const file of filesToTouch) {
+  const p = path.join(dst, file);
+  mkdirpSync(path.dirname(p));
+  fs.writeFileSync(p, '');
+}
+
+const pkgJson = {
+  name: '@tmattio/spin',
+  version: package.version,
+  description: package.description,
+  homepage: package.homepage,
+  scripts: {
+    postinstall: 'node ./postinstall.js'
+  },
+  bin: {
+    spin: 'spin.exe'
+  },
+  files: [
+    'platform-linux-x64/',
+    'platform-darwin-x64/',
+    'postinstall.js',
+    'spin.exe'
+  ]
+};
+
+fs.writeFileSync(path.join(dst, 'package.json'), JSON.stringify(pkgJson, null, 2));
