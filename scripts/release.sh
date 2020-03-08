43cde2c8f652f695f1bf62ee7598b39d02c6b526
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-03-08 16:41:23 -0400

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-function bump_source() {
-  search='(let version = ").+(")'
-  replace="\1$2\2"
-  sed -i ".tmp" -E "s/${search}/${replace}/g" "$1"
-  rm "$1.tmp"
-}
-
-function bump_all() {
-  output=$(npm version "${release}" --no-git-tag-version)
-  version=${output:1}
-  bump_source "bin/package.re" "$version"
-}
-
-version=$(sed -nE 's/^version: "(.*)"$/\1/p' inquire.opam)
-
 if [ -d ".git" ]; then
   changes=$(git status --porcelain)
   branch=$(git rev-parse --abbrev-ref HEAD)
@@ -26,16 +11,13 @@ if [ -d ".git" ]; then
     echo "Please run the release script on master"
     exit 1
   else
-    bump_all
-    git add .
-    git commit -m "Bump to ${version}"
-    dune-release tag ${version} -y
-    dune-release distrib
-    dune-release publish -y
-    dune-release opam submit --no-auto-open -y
-    git tag -a "${output}" -m "${version}"
-    git push origin --tags
+    esy dune-release tag
+    esy dune-release distrib
+    esy dune-release publish -y
+    esy dune-release opam pkg
+    esy dune-release opam submit --no-auto-open -y
   fi
 else
-  bump_all
+  echo "This project is not a git repository. Run `git init` first to be able to release."
+  exit 1
 fi
