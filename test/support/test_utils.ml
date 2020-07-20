302c3f80a16f111249835f1e6df28ec76a6eb7bb
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-07-20 09:33:43 +0200

    Inline Alcotest dependency

--- test/support/test_utils.ml
+++ test/support/test_utils.ml
@@ -1,19 +1,20 @@
-open Alcotest
 open Spin
 
-let decoder_error = of_pp Decoder.pp_error
+let decoder_error = Alcotest.of_pp Decoder.pp_error
 
 let decoder a =
   let eq x y =
     match x, y with
     | Ok x, Ok y ->
-      equal a x y
+      Alcotest.equal a x y
     | Error x, Error y ->
-      equal decoder_error x y
+      Alcotest.equal decoder_error x y
     | _ ->
       false
   in
-  testable (Fmt.Dump.result ~ok:(pp a) ~error:(pp decoder_error)) eq
+  Alcotest.testable
+    (Fmt.Dump.result ~ok:(Alcotest.pp a) ~error:(Alcotest.pp decoder_error))
+    eq
 
 let get_tempdir prefix =
   Printf.sprintf "%s-%s" prefix (Unix.time () |> Float.to_int |> Int.to_string)
