19b7f0a8cfa69014ff82a6e314d157d55051906e
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-08-05 14:17:31 +0200

    Add a unit test for the home page rendering

--- template/spa/template/test/page_home_test.ml
+++ template/spa/template/test/page_home_test.ml
@@ -1,27 +1,58 @@
-open Alcotest
-open Core_kernel
-open Incr_dom_testing
+open! Incr_dom_testing
+open Demo
 
-let%expect_test _ =
+let%expect_test "can render home page" =
   let driver =
     Driver.create
-      ~initial_model:Demo.App.initial_model
-      ~sexp_of_model:ADemo.pp.Model.sexp_of_t
+      ~initial_model:Page_home.Model.empty
       ~initial_state:()
-      (module Demo.App)
+      ~sexp_of_model:Page_home.Model.sexp_of_t
+      (module Page_home)
   in
   let module H = (val Helpers.make driver) in
   H.show_view ();
-  [%expect {|
-    <body>
-      <div> 0 </div>
-    </body> |}];
-  H.do_actions [ App.Action.Increment ];
+  [%expect
+    {|
+    <div>
+      <div class="text-center mt-12">
+        <p class="text-3xl text-gray-900 mb-4">
+          👋 Welcome Visitor! You can edit me in
+          <code>
+
+      lib/components/greet.{% if syntax == 'Reason' %}re{% else %}ml{% endif %}
+          </code>
+        </p>
+        <p class="text-xl text-gray-900 mb-4"> Here a simple counter example that you can look at to get started: </p>
+        <div class="space-x-6">
+          <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500" onclick={handler}> - </button>
+          <span class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"> 0 </span>
+          <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500" onclick={handler}> + </button>
+        </div>
+      </div>
+    </div>
+|}];
+  H.do_actions [ Page_home.Action.Greet_action Greet.Action.Increment ];
   H.perform_update ();
   H.show_model ();
-  [%expect {| ((counter 1)) |}];
+  [%expect {| ((greet_model 1)) |}];
   H.show_view ();
-  [%expect {|
-    <body>
-      <div> 1 </div>
-    </body> |}]
+  [%expect
+    {|
+    <div>
+      <div class="text-center mt-12">
+        <p class="text-3xl text-gray-900 mb-4">
+          👋 Welcome Visitor! You can edit me in
+          <code>
+
+      lib/components/greet.{% if syntax == 'Reason' %}re{% else %}ml{% endif %}
+          </code>
+        </p>
+        <p class="text-xl text-gray-900 mb-4"> Here a simple counter example that you can look at to get started: </p>
+        <div class="space-x-6">
+          <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500" onclick={handler}> - </button>
+          <span class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"> 1 </span>
+          <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500" onclick={handler}> + </button>
+        </div>
+      </div>
+    </div>
+|}]
