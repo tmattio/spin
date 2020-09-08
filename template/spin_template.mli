5e73745e5e8ec7d04000c57a2114473e2e1e301b
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-09-09 00:16:16 +0200

    Remove unused module definitions

--- template/spin_template.mli
+++ template/spin_template.mli
@@ -6,15 +6,5 @@ module type Template = sig
   val read : string -> string option
 end
 
-module Bs_react : Template
-
-module Cli : Template
-
-module Lib : Template
-
-module Bin : Template
-
-module Ppx : Template
-
 val all : (module Template) list
 (** List of all of the official spin templates *)
