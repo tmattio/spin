cd7814357d7c425eef9791e62ba9a28df3cd30f6
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-10-04 18:47:13 +0200

    Remove dependency on stdio

--- test/spin_std/spin_sys_test.ml
+++ test/spin_std/spin_sys_test.ml
@@ -1,11 +1,12 @@
 open Alcotest
-open Spin_std
 
 let test_ls_dir () =
-  let temp_dir = Spin_sys.mk_temp_dir "spin-test" in
+  let temp_dir = Spin_std.Spin_sys.mk_temp_dir "spin-test" in
   let temp_file = Filename.concat temp_dir "file" in
-  Stdio.Out_channel.write_all temp_file ~data:"";
-  let f_list = Spin_sys.ls_dir (Caml.Filename.dirname temp_file) in
+  let oc = open_out temp_file in
+  Printf.fprintf oc "%s" "Hello world!";
+  close_out oc;
+  let f_list = Spin_std.Spin_sys.ls_dir (Caml.Filename.dirname temp_file) in
   check (list string) "equals" f_list [ temp_file ]
 
 let suite = [ "ls_dir returns the files in the directory", `Quick, test_ls_dir ]
