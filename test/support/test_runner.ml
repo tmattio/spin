74ff82ee593326e706610574d4d074fb2e805ffc
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-10-04 18:17:29 +0200

    Add back unit tests

--- test/support/test_runner.ml
+++ test/support/test_runner.ml
@@ -4,15 +4,14 @@ open Spin_test
 
     This aggregates all the test suites and call Alcotes to run them. When
     creating a new test suite, don't forget to add it here! *)
-    
+
 let () =
   Alcotest.run
     "spin"
-    [
-       (* "spin::Dec_common", Dec_common_test.suite *)
-    (* ; "spin::Dec_user_config", Dec_user_config_test.suite *)
-    (* ; "spin::Decoder", Decoder_test.suite *)
-    (* Disable because it depends on Reason. Only uncomment in development. *)
-     "spin::Template_actions", Template_actions_test.suite
-    (* ; "spin_std::Spin_sys", Spin_sys_test.suite *)
+    [ "spin::Dec_common", Dec_common_test.suite
+    ; "spin::Dec_user_config", Dec_user_config_test.suite
+    ; "spin::Decoder", Decoder_test.suite
+    ; "spin_std::Spin_sys", Spin_sys_test.suite
+      (* Disable because it depends on Reason. Only uncomment in development. *)
+      (* ; "spin::Template_actions", Template_actions_test.suite *)
     ]
