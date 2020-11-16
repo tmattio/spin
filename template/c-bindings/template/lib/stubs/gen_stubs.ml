ed05fae5738b40ec60f5c2869157289421170a0c
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-11-16 09:32:34 +0100

    Fix broken templates

--- template/c-bindings/template/lib/stubs/gen_stubs.ml
+++ template/c-bindings/template/lib/stubs/gen_stubs.ml
@@ -15,6 +15,6 @@ let () =
     | _ ->
       assert false
   in
-  fn ~concurrency:Cstubs.unlocked format ~prefix:"{{ project_snake }}" (module Demo_stubs.Def);
+  fn ~concurrency:Cstubs.unlocked format ~prefix:"{{ project_snake }}" (module {{ project_snake | capitalize }}_stubs.Def);
   Format.pp_print_flush format ();
   close_out oc
--- template/js/template/bin/main.ml
+++ template/js/template/bin/main.ml
@@ -1,3 +1,3 @@
 let elt = Js_of_ocaml.Dom_html.getElementById_exn "root"
 
-let () = Demo.App.inject elt
+let () = {{ project_snake | capitalize }}.App.inject elt
